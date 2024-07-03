90190c691b191c736a416bb199196620c3b2eb9c
dodollama <162728301+dodollama@users.noreply.github.com>
2024-07-03 14:39:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,8 +51,21 @@ function customResponse(input) {
   } else if (input.startsWith('hi my name is')) {
     const name = input.substring(13).trim();
     return `Hi ${name}!`;
-  } else if (input === 'tell me a story' || input === 'tell story' || input === 'tell a story' || input === 'write me a poem' || input === 'write poem' || input === 'write a poem' || input === 'tell me a poem' || input === 'tell poem' || input === 'tell a poem' || input === 'write me a story' || input === 'write story' || input === 'write a story') {
-    return 'Sorry! I can not do this at the point of time!';
+  } else if ([
+    'tell me a story',
+    'tell story',
+    'tell a story',
+    'write me a poem',
+    'write poem',
+    'write a poem',
+    'tell me a poem',
+    'tell poem',
+    'tell a poem',
+    'write me a story',
+    'write story',
+    'write a story'
+  ].includes(input)) {
+  return 'Sorry! I can not do this at the point of time!';
   } else if (input.startsWith('hello my name is')) {
     const name = input.substring(13).trim();
     return `Hi ${name}!`;
